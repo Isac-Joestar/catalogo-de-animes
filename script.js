24ba7e7b02bfd1030417df3d37da518dad71b49a
Isac Mikael <109987080+Isac-Joestar@users.noreply.github.com>
2023-03-05 19:51:27 -0300

    Upload dos arquivos

---
+++ script.js
@@ -0,0 +1,77 @@
+var sun = document.querySelector('#light')
+var moon = document.querySelector('#dark')
+var temaIcon = document.querySelectorAll(".temaIcon")
+temaIcon.forEach((t)=>{
+    // console.log(t)
+    t.addEventListener('click', ()=>{
+       
+        if(sun.style.display == 'flex'){
+            sun.style.display = 'none'
+            moon.style.display = 'flex'
+            Tema('light')
+           
+        }else if(moon.style.display = 'flex'){
+            sun.style.display = 'flex'
+            moon.style.display = 'none'
+            Tema('dark')
+        }
+    })
+})
+
+function Tema(a){
+    
+    if(a == 'light'){
+        document.querySelector('body').classList.add('light-mode-bg')
+
+        document.querySelector('header').classList.add('light-mode-bg')
+
+        document.querySelectorAll('#mangas span')
+        .forEach((t)=>{
+            t.classList.add('light-mode-txt')
+        })
+        
+        temaIcon
+        .forEach((t)=>{
+            t.classList.add('light-mode-txt')
+        })
+
+        document.querySelector('#pesquisa label span')
+        .classList.add('light-mode-txt')
+
+    }else if(a == 'dark'){
+         document.querySelector('body').classList.remove('light-mode-bg')
+
+        document.querySelector('header').classList.remove('light-mode-bg')
+
+        document.querySelectorAll('#mangas span')
+        .forEach((t)=>{
+            t.classList.remove('light-mode-txt')
+        })
+        
+        temaIcon
+        .forEach((t)=>{
+            t.classList.remove('light-mode-txt')
+        })
+
+        document.querySelector('#pesquisa label span')
+        .classList.remove('light-mode-txt')
+        
+        
+    }
+}
+
+// SlIDER
+
+document.getElementById("radio1").checked = true;
+let count = 1
+setInterval(()=>{
+    nextImage()
+},5000)
+
+function nextImage(){
+    count ++;
+    if(count > 4){
+        count = 1
+    }
+    document.getElementById("radio"+ count).checked = true;
+}
