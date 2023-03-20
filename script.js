5597d4385824c118059acd647f74edd39133aa36
Isac Mikael <109987080+Isac-Joestar@users.noreply.github.com>
2023-03-19 21:16:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -111,6 +111,7 @@ const scrollManga = document.querySelectorAll('.mangas_content')
 
 // API mangas
 const urlEmAlta = 'https://api.jikan.moe/v4/manga?page=1&order_by=popularity' 
+// const urlEmAlta = 'https://api.jikan.moe/v4/manga/news' 
 const urlLancamentos = 'https://api.jikan.moe/v4/manga?page=1&status=publishing' 
 // -H 'Authorization: Bearer YOUR_TOKEN'
 
